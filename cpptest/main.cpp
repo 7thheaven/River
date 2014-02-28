b8647c5a3ada12f9608e51dd7ce56f9347bc3456
Little7 <7thheaven177@gmail.com>
2014-02-28 10:42:24 +0800

    leetcode011

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -5,37 +5,9 @@
 
 using namespace std;
 
-int lengthOfLongestSubstring(string s) {
-        int len=s.length();
-        if(!len)
-            return 0;
-        int res=0,temp=0;
-        bool has;
-        for(int i=0;i<len;++i)
-        {
-            has=false;
-            for(int j=0;j<temp;++j)
-            {
-                if(s[i-j-1]==s[i])
-                {
-                    if(res<temp)
-                        res=temp;
-                    temp=j+1;
-                    has=true;
-                    break;
-                }
-            }
-            if(!has)
-                ++temp;
-        }
-        if(temp>res)
-            res=temp;
-        return res;
-    }
-
 int main()
 {
-    string a="wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybldbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco";
-    cout<<lengthOfLongestSubstring(a)<<endl;
+    int a=1052254545;
+    cout<<a*2<<endl;
     return 0;
 }
