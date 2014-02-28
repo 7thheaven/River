0b2f9426e6338bf8cfb9fad24ed0799a5c3319ed
Little7 <7thheaven177@gmail.com>
2014-02-28 22:24:56 +0800

    leetcode017

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -2,26 +2,20 @@
 #include <cstdio>
 #include <cmath>
 #include <map>
+#include <vector>
 
 using namespace std;
-
-bool isPalindrome(int x) {
-        if(x<0)
-            return false;
-        while(x)
-        {
-            if(x/int(pow(10,int(log10(x))))!=x%10)
-                return false;
-            if(int(log10(x)))
-                x/=pow(10,int(log10(x))-int(log10(x-(x/int(pow(10,int(log10(x)))))*pow(10,int(log10(x))))));
-            x-=(x/int(pow(10,int(log10(x)))))*int(pow(10,int(log10(x))));
-        }
-        return true;
-    }
 
 int main()
 {
     int x=1;
-    cout<<isPalindrome(x)<<endl;
+    vector<vector<int> > a;
+    for(int i=0;i<10;++i)
+    {
+        vector<int> b;
+        b.push_back(x);
+        a.push_back(b);
+    }
+    cout<<x<<" "<<a[2][0]<<" "<<a.size()<<" "<<a[7].size()<<endl;
     return 0;
 }
